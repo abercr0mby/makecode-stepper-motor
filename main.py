53847b672d705a4dc22091fa9141eed141522f98
abercr0mby <abercromby@gmail.com>
2020-08-20 13:52:21 +0100

    tilt left and tilt right

--- main.py
+++ main.py
@@ -8,17 +8,34 @@ def move_motor(is_forward: bool, steps: number):
 
 def on_button_pressed_a():
     move_motor(True, default_steps)
-    basic.pause(100)
     move_motor(False, default_steps)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_tilt_left():
+    move_motor(True, default_steps)
+    move_motor(False, default_steps)
+input.on_gesture(Gesture.TILT_LEFT, on_tilt_left)
+
+def on_tilt_right():
+    move_motor(True, 10)
+    move_motor(False, 10)
+    move_motor(True, 10)
+    move_motor(False, 10)
+    move_motor(True, 10)
+    move_motor(False, 10)
+    move_motor(True, 10)
+    move_motor(False, 10)     
+    move_motor(True, 10)
+    move_motor(False, 10)     
+    move_motor(True, 10)
+    move_motor(False, 10)    
+    move_motor(True, 10)
+    move_motor(False, 10)                         
+input.on_gesture(Gesture.TILT_RIGHT, on_tilt_right)
+
 def move_motor_one_step(is_forward: bool):
     global pause_for
-    pause_for = 10
-    pins.digital_write_pin(DigitalPin.P0, 0)
-    pins.digital_write_pin(DigitalPin.P1, 0)
-    pins.digital_write_pin(DigitalPin.P2, 0)
-    pins.digital_write_pin(DigitalPin.P3, 0)
+    pause_for = 0.5
     if is_forward:
         basic.pause(pause_for)
         pins.digital_write_pin(DigitalPin.P0, 1)
@@ -63,8 +80,19 @@ def move_motor_one_step(is_forward: bool):
         pins.digital_write_pin(DigitalPin.P2, 0)
         pins.digital_write_pin(DigitalPin.P3, 0)
         basic.pause(pause_for)
+
+def on_button_pressed_b():
+    move_motor(False, 5)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 pause_for = 0
 current_direction = False
 count = 0
+default_steps = 0
+bluetooth.start_accelerometer_service()
+bluetooth.start_button_service()
+bluetooth.start_io_pin_service()
+bluetooth.start_led_service()
+bluetooth.start_temperature_service()
 led.enable(False)
-default_steps = 50
+default_steps = 40
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-function move_motor (is_forward: boolean, steps: number) {
+function move_motor(is_forward: boolean, steps: number) {
+    
     count = 0
     current_direction = is_forward
     while (count < steps && current_direction == is_forward) {
@@ -6,17 +7,34 @@ function move_motor (is_forward: boolean, steps: number) {
         move_motor_one_step(is_forward)
     }
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     move_motor(true, default_steps)
-    basic.pause(100)
     move_motor(false, default_steps)
 })
-function move_motor_one_step (is_forward: boolean) {
-    pause_for = 1
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P3, 0)
+input.onGesture(Gesture.TiltLeft, function on_tilt_left() {
+    move_motor(true, default_steps)
+    move_motor(false, default_steps)
+})
+input.onGesture(Gesture.TiltRight, function on_tilt_right() {
+    move_motor(true, 10)
+    move_motor(false, 10)
+    move_motor(true, 10)
+    move_motor(false, 10)
+    move_motor(true, 10)
+    move_motor(false, 10)
+    move_motor(true, 10)
+    move_motor(false, 10)
+    move_motor(true, 10)
+    move_motor(false, 10)
+    move_motor(true, 10)
+    move_motor(false, 10)
+    move_motor(true, 10)
+    move_motor(false, 10)
+})
+function move_motor_one_step(is_forward: boolean) {
+    
+    pause_for = 0.5
     if (is_forward) {
         basic.pause(pause_for)
         pins.digitalWritePin(DigitalPin.P0, 1)
@@ -62,8 +80,10 @@ function move_motor_one_step (is_forward: boolean) {
         pins.digitalWritePin(DigitalPin.P3, 0)
         basic.pause(pause_for)
     }
+    
 }
-input.onButtonPressed(Button.B, function () {
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     move_motor(false, 5)
 })
 let pause_for = 0
@@ -76,4 +96,4 @@ bluetooth.startIOPinService()
 bluetooth.startLEDService()
 bluetooth.startTemperatureService()
 led.enable(false)
-default_steps = 50
+default_steps = 40
