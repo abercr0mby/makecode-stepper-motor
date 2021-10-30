732376aec019f962f910bbba77d7ee854844088f
abercr0mby <abercromby@gmail.com>
2021-10-30 20:04:50 +0100

    Added tilt config

--- main.py
+++ main.py
@@ -1,37 +1,3 @@
-def on_button_pressed_a():
-    toggle_talk()
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    global stop
-    stop = False
-    move_motor(False, 5)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_tilt_left():
-    toggle_talk()
-input.on_gesture(Gesture.TILT_LEFT, on_tilt_left)
-
-def on_tilt_right():
-    toggle_talk()                  
-input.on_gesture(Gesture.TILT_RIGHT, on_tilt_right)
-
-
-def talk():
-    global stop
-    while True:
-        if not stop:
-            open_and_close_mouth(randint(1, 10) + randint(1, 10)) 
-        basic.pause(100)           
-
-def toggle_talk():
-    global stop
-    stop = not stop
-
-def open_and_close_mouth(steps: int):
-    move_motor(True, steps)
-    move_motor(False, steps)
-
 def move_motor(is_forward: bool, steps: number):
     global count, current_direction
     count = 0
@@ -40,10 +6,14 @@ def move_motor(is_forward: bool, steps: number):
         count = count + 1
         move_motor_one_step(is_forward)
 
-def move_motor_one_step(is_forward: bool):
+def on_button_pressed_a():
+    toggle_talk()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def move_motor_one_step(is_forward2: bool):
     global pause_for
     pause_for = 0.5
-    if is_forward:
+    if is_forward2:
         basic.pause(pause_for)
         pins.digital_write_pin(DigitalPin.P0, 1)
         pins.digital_write_pin(DigitalPin.P1, 0)
@@ -87,11 +57,41 @@ def move_motor_one_step(is_forward: bool):
         pins.digital_write_pin(DigitalPin.P2, 0)
         pins.digital_write_pin(DigitalPin.P3, 0)
         basic.pause(pause_for)
+def toggle_talk():
+    global stop
+    stop = not (stop)
 
-default_steps = 10
+def on_gesture_tilt_left():
+    if tilt_enabled:
+            toggle_talk()
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def talk():
+    while True:
+        if not (stop):
+            open_and_close_mouth(randint(1, 10) + randint(1, 10))
+        basic.pause(100)
+def open_and_close_mouth(steps2: number):
+    move_motor(True, steps2)
+    move_motor(False, steps2)
+
+def on_button_pressed_b():
+    global stop
+    stop = False
+    move_motor(False, 5)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_tilt_right():
+    if tilt_enabled:
+        toggle_talk()
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
 pause_for = 0
 current_direction = False
 count = 0
+stop = False
+tilt_enabled = False
+default_steps = 10
 bluetooth.start_accelerometer_service()
 bluetooth.start_button_service()
 bluetooth.start_io_pin_service()
@@ -99,5 +99,4 @@ bluetooth.start_led_service()
 bluetooth.start_temperature_service()
 led.enable(False)
 stop = True
-
 talk()
--- main.ts
+++ main.ts
@@ -1,38 +1,3 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    toggle_talk()
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    stop = false
-    move_motor(false, 5)
-})
-input.onGesture(Gesture.TiltLeft, function on_tilt_left() {
-    toggle_talk()
-})
-input.onGesture(Gesture.TiltRight, function on_tilt_right() {
-    toggle_talk()
-})
-function talk() {
-    
-    while (true) {
-        if (!stop) {
-            open_and_close_mouth(randint(1, 10) + randint(1, 10))
-        }
-        
-        basic.pause(100)
-    }
-}
-
-function toggle_talk() {
-    
-    stop = !stop
-}
-
-function open_and_close_mouth(steps: number) {
-    move_motor(true, steps)
-    move_motor(false, steps)
-}
-
 function move_motor(is_forward: boolean, steps: number) {
     
     count = 0
@@ -43,10 +8,13 @@ function move_motor(is_forward: boolean, steps: number) {
     }
 }
 
-function move_motor_one_step(is_forward: boolean) {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    toggle_talk()
+})
+function move_motor_one_step(is_forward2: boolean) {
     
     pause_for = 0.5
-    if (is_forward) {
+    if (is_forward2) {
         basic.pause(pause_for)
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
@@ -94,15 +62,54 @@ function move_motor_one_step(is_forward: boolean) {
     
 }
 
-let default_steps = 10
+function toggle_talk() {
+    
+    stop = !stop
+}
+
+input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+    if (tilt_enabled) {
+        toggle_talk()
+    }
+    
+})
+function talk() {
+    while (true) {
+        if (!stop) {
+            open_and_close_mouth(randint(1, 10) + randint(1, 10))
+        }
+        
+        basic.pause(100)
+    }
+}
+
+function open_and_close_mouth(steps2: number) {
+    move_motor(true, steps2)
+    move_motor(false, steps2)
+}
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    stop = false
+    move_motor(false, 5)
+})
+input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+    if (tilt_enabled) {
+        toggle_talk()
+    }
+    
+})
 let pause_for = 0
 let current_direction = false
 let count = 0
+let stop = false
+let tilt_enabled = false
+let default_steps = 10
 bluetooth.startAccelerometerService()
 bluetooth.startButtonService()
 bluetooth.startIOPinService()
 bluetooth.startLEDService()
 bluetooth.startTemperatureService()
 led.enable(false)
-let stop = true
+stop = true
 talk()
